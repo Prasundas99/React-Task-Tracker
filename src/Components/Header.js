12a1aa78079a8def3ea08877e728c5f2f37a3608
Prasundas99 <prasundas115@gmail.com>
2021-02-06 20:51:02 +0530

    upto events

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import Button from "./Button";
+
+
+const Header = () => {
+
+   const onClick = () => {
+       
+   }
+
+
+    return (
+        <header className='header'>
+            <h1>Task Tracker</h1>
+            <Button onClick={onClick}/>
+        </header>
+    )
+}
+
+export default Header
